651d8512aceaef857f5402b5f5f91e1ef601efbe
Daria Kolodzey <kolodaria@google.com>
2016-04-06 20:24:38 +0300

    Add lab0 solution

---
+++ lab0/lab0.fs
@@ -0,0 +1,35 @@
+module lab0
+
+open System
+open System.Net
+open System.Collections.Specialized
+
+let (email, name) = ("dkolodzey@ya.ru", "Kolodzey D.A.") // адрес почты и фамилия с инициалами
+
+let rec pascal c r =
+    match (c, r) with
+    | (c, r) when c = 0 -> 1
+    | (c, r) when c = r -> 1
+    | _ -> ((pascal c (r-1)) + (pascal (c-1) (r-1)))
+
+let printIt n = 
+  "[" +
+  ([for x in 0..n do for y in 0..x do yield pascal y x] 
+    |> List.map (fun x -> x.ToString())
+    |> List.reduce (fun x y -> x + "," + y) )
+  + "]"
+
+let main () = 
+  let values = new NameValueCollection()
+  values.Add("email", email)
+  values.Add("name", name)
+  values.Add("content", printIt 20)
+
+  let client = new WebClient()
+  let response = client.UploadValues(new Uri("http://91.239.143.158:13666/lab0"), values)
+  let responseString = Text.Encoding.Default.GetString(response)
+
+  printf "%A\n" responseString
+
+
+main();;
